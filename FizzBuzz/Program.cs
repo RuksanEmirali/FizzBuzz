15b1ee345e25f20c1d4e92664e3206e18a65eb95
ruksan_emirali <ruksanemirali@gmail.com>
2024-08-05 10:59:17 +0100

    creates loop to log int 1-100

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,13 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-namespace FizzBuzz
+﻿for (var counter = 1; counter <101; counter++)
 {
-    class Program 
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine(counter);
 }
+
--- FizzBuzz/obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ FizzBuzz/obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781e94d40542cd2551ec251eea6da1a3c7fcafc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0253ade78caf94f970a300a889789f42f79da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
